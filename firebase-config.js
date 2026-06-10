d4ac031596690f03935f8ed0ad02793ef966dc8a
@angelocamargo <angelocamargo@se-pmmc.com.br>
2026-06-10 14:11:20 -0300

    Added firebase-config.js file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDwz4QejDcr8q9EAH5z3U_r-qaQIhZ1Nks",
+  authDomain: "friendlychat-c669e.firebaseapp.com",
+  projectId: "friendlychat-c669e",
+  storageBucket: "friendlychat-c669e.firebasestorage.app",
+  messagingSenderId: "356562330974",
+  appId: "1:356562330974:web:0d059a8e041eb867dd5bcf"
+};
